6db72e3f38770384d5bc6b5f2448987d6d3c72f5
Regan Boreland <reganboreland@hotmail.co.uk>
2024-06-02 16:11:34 +0000

    add function names to script.js

---
+++ assets/javascript/script.js
@@ -0,0 +1,23 @@
+function howToPlay(){
+
+}
+
+function username(){
+
+}
+
+function ancientWondersGame(){
+
+}
+
+function modernWondersGame(){
+
+}
+
+function correctScore(){
+
+}
+
+function incorrectScore(){
+
+}
